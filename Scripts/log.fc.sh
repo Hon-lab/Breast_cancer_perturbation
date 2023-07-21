53ef7ac7471c66e0e536489e4ca153a2dc954b31
Yihan Wang <50383583+yihan1119@users.noreply.github.com>
2023-07-21 14:16:50 -0500

    Update log.fc.sh

--- Scripts/log.fc.sh
+++ Scripts/log.fc.sh
@@ -10,23 +10,13 @@
 #SBATCH --mail-type=end                                   # send email when job status change (start, end, abortion and etc.)
 
 setenv PATH /project/GCRB/Hon_lab/s426305/Conda/py37-cluster2/bin:$PATH
-set ALL_DICT=/project/GCRB/Hon_lab/s426305/Sequencing_data_analysis/10X/MB231-GWAS_SM-combine/All_enhancer_regions_hg38.txt
-set SM_DICT=/project/GCRB/Hon_lab/s426305/Analysis/Mosaic-seq/MB231-dCas9-KRAB+YWsg2P1/annotation/enhancer_regions_hg38.txt
+
 set GWAS_DICT=/project/GCRB/Hon_lab/s426305/Analysis/Mosaic-seq/MB231-dCas9-KRAB+YWsg1P3/enhancer_regions_combined.txt
 set MB231_GWAS_DIR=/project/GCRB/Hon_lab/s426305/Analysis/Spade_test/MB231/MB231_GWAS/
-set MB231_SM_DIR=/project/GCRB/Hon_lab/s426305/Analysis/Spade_test/MB231/MB231_SM/
 
 #GWAS
-#pySpade fc \
-#	-t $MB231_GWAS_DIR\
-#	-d $GWAS_DICT\
-#	-r ./GWAS_repression_query.txt\
-#	-o ./Jupyter_plots/
-
-
-#SM
- pySpade fc \
-	-t $MB231_SM_DIR\
-	-d $SM_DICT\
-	-r ./SM_repression_query.txt\
+pySpade fc \
+	-t $MB231_GWAS_DIR\
+	-d $GWAS_DICT\
+	-r ./GWAS_repression_query.txt\
 	-o ./Jupyter_plots/
