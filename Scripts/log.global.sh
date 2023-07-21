810d95c7419e7d45b4b1d9f180cba3dcfea27afe
Yihan Wang <50383583+yihan1119@users.noreply.github.com>
2023-07-21 14:20:35 -0500

    Update log.global.sh

--- Scripts/log.global.sh
+++ Scripts/log.global.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 
-#SBATCH --job-name=Spade_global_CMpilot2_cluster0_ver0    # job name
+#SBATCH --job-name=pySpade_global_MB231_GWAS              # job name
 #SBATCH --partition=256GB,256GBv1                         # select partion from 128GB, 256GB, 384GB, GPU and super
 #SBATCH --nodes=1                                         # number of nodes requested by user
 #SBATCH --time=60-00:00:00                                # run time, format: D-H:M:S (max wallclock time)
@@ -10,15 +10,15 @@
 #SBATCH --mail-type=end                                   # send email when job status change (start, end, abortion and etc.)
 
 setenv PATH /project/GCRB/Hon_lab/s426305/Conda/py37-cluster2/bin:$PATH
-set DIR=/project/GCRB/Hon_lab/s426305/Analysis/IGVF/20230612_CMPilot2_cluster0_ver0/pySpade/
-set OBS_DIR=$DIR/CMPilot2_cluster0_ver0_DEobs/
-set RAND_DIR=$DIR/CMPilot2_cluster0_ver0_DErand/
-set SGRNA_DICT=$DIR/sgRNA_dict_hg38.txt
+set DIR=/project/GCRB/Hon_lab/s426305/Analysis/Spade_test/MB231/
+set OBS_DIR=$DIR/MB231_GWAS_DEobs/
+set RAND_DIR=$DIR/MB231_GWAS_DErand/
+set SGRNA_DICT=/project/GCRB/Hon_lab/s426305/Analysis/Mosaic-seq/MB231-dCas9-KRAB+YWsg2P1/annotation/enhancer_regions_hg38.txt
  
  pySpade global \
 	-f $DIR \
 	-d $OBS_DIR\
 	-s $SGRNA_DICT\
 	-t $RAND_DIR\
-	-o ./CMpilot2_cluster0_ver0_global_hit.csv
+	-o ./MB231_GWAS_global_hit.csv
 
